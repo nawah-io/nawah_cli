4db9601a8622c18286625bafa8fd4af1e5ea179a
Mahmoud Abduljawad <mahmoud@masaar.com>
2020-06-03 12:30:20 +0000

    Updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setuptools.setup(
 	name='nawah_cli',
-	version='0.1.0',
+	version='1.0.0',
 	author='Mahmoud Abduljawad',
 	author_email='mahmoud@masaar.com',
 	description='CLI for Nawah--Rapid app development framework',
