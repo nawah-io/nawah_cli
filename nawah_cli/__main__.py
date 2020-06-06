00be5a61d66271ae2409f1849c74f8af4cfd9d6d
Mahmoud Abduljawad <mahmoud@masaar.com>
2020-06-06 13:10:14 +0400

    CLI v1.0.2 Release (#3)
    - Refactore to using wheel when creating new app.
    - Dropped unnecessary files from repo.
    - Bumped version to 1.0.2.

--- nawah_cli/__main__.py
+++ nawah_cli/__main__.py
@@ -1,10 +1,14 @@
 import os
 
 def main():
-	if os.path.exists(os.path.join('.', 'nawah', 'cli.py')):
-		import sys, subprocess
-		subprocess.call([sys.executable, '.'] + sys.argv[1:])
-		exit()
+	if os.path.exists(os.path.join('.', 'nawah_app.py')):
+		# [REF] https://stackoverflow.com/a/3964691/2393762
+		import sys, glob
+		whl_name = glob.glob('nawah-*.whl')[0]
+		# [REF] http://avrilomics.blogspot.com/2015/11/import-python-module-from-egg-file.html
+		sys.path.insert(0, os.path.join('.', whl_name))
+		from nawah.cli import nawah_cli
+		nawah_cli()
 	else:
 		from nawah_cli.cli import nawah_cli
 		nawah_cli()
